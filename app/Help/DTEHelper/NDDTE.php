4f5ba1db4b3e35a0e34b3dbd70a49050bfa3697c
Cristian Maltez <cristianjosuemaltezrivas@gmail.com>
2024-11-18 22:20:27 -0600

    estructura endpoint nota de debito sin probar

---
+++ app/Help/DTEHelper/NDDTE.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Help\DTEHelper;
+
+use App\Help\Generator;
+use App\help\Help;
+
+class NDDTE
+{
+
+    public static function extension($observacion)
+    {
+        return [
+            "docuEntrega" => null,
+            "observaciones" => $observacion,
+            "nombRecibe" => null,
+            "nombEntrega" => null,
+            "docuRecibe" => null,
+        ];
+    }
+
+    public static function cuerpo($cuerpo)
+    {
+
+
+        if ($cuerpo == null)
+            return null;
+
+        // AGREGAR NUMERO DE ITEM
+        foreach ($cuerpo as $key => $item) {
+            $cuerpo[$key]['numItem'] = $key + 1;
+        }
+
+        return $cuerpo;
+    }
+
+    public static function resumen($cuerpo, $tipoCliente, $pagoTributos)
+    {
+        $resumen = [];
+
+        // VARIABLES PARA GUARDAR CALCULOS APARTIR DE LOS ITEMS DEL CUERPO DEL DOCUMENTO
+        $totalExenta = 0.0;
+        $totalNoSuj = 0.0;
+        $totalGravada = 0.0;
+        $totalDescu = 0.0;
+        $montoTotalOperacion = 0.0;
+
+        $descuNoSuj = 0.0;
+        $descuExenta = 0.0;
+        $descuGravada = 0.0;
+
+        $subTotal = 0.0;
+
+        $ivaPerci1 = 0.0;
+        $ivaRetenida = 0.0;
+        $reteRenta = 0.0;
+
+        $tributos = [];
+
+        // VARIABLES DE PROCESO PARA GUARDAR VALORES A USAR AFUERA DEL CICLO FOREACH
+        $totalImpuestos = 0.0;
+
+        foreach ($cuerpo as $key => $value) {
+            $ventaGravada = $value['ventaGravada'];
+            $impuestoTotalItem = 0.0;
+
+            $totalNoSuj += $value['ventaNoSuj'];
+            $totalExenta += $value['ventaExenta'];
+            $totalGravada += $value['ventaGravada'];
+            $subTotal += $ventaGravada;
+
+            $totalDescu += $value['montoDescu'];
+
+            $ventaSinDescuento = round(($value['precioUni'] * $value['cantidad']), 2);
+
+            // Calcular el IVA retenido si aplica
+            if ($tipoCliente == 3 && $ventaSinDescuento >= 100) {
+                $ivaRetenidoItem = round($ventaSinDescuento * 0.01, 2);
+                $ivaRetenida += $ivaRetenidoItem;
+            }
+
+            // Procesar tributos si existen
+            if ($pagoTributos != null) {
+                $pagoTributo = $pagoTributos[$key];
+
+                foreach ($pagoTributo as $keyObjec => $valorObjec) {
+                    $totalImpuestos += $valorObjec;
+                    $impuestoTotalItem += $valorObjec;
+
+                    // Buscar si el tributo ya existe en el array
+                    $clave = array_search($keyObjec, array_column($tributos, 'codigo'));
+
+                    if ($clave !== false) {
+                        // Si el tributo ya existe, actualizar su valor
+                        $tributos[$clave]['valor'] += $valorObjec;
+                        continue;
+                    }
+
+                    // Si no existe, agregar un nuevo tributo al array
+                    $tributos[] = [
+                        'codigo' => strval($keyObjec),
+                        'descripcion' => Help::getTributo($keyObjec),
+                        'valor' => round($valorObjec, 2)
+                    ];
+                }
+            }
+        }
+
+        $subTotal = round($subTotal, 2);
+        $montoTotalOperacion = round($subTotal + $totalImpuestos - $ivaRetenida, 2);
+
+        $numero_en_letras = Generator::generateStringFromNumber($montoTotalOperacion);
+
+
+        $resumen = [
+            'totalNoSuj' => $totalNoSuj,
+            'totalExenta' => $totalExenta,
+            'totalGravada' => $totalGravada,
+            'totalDescu' => $totalDescu,
+            'montoTotalOperacion' => $montoTotalOperacion,
+
+            'descuNoSuj' => $descuNoSuj,
+            'descuGravada' => $descuGravada,
+            'descuExenta' => $descuExenta,
+
+            'subTotalVentas' => $subTotal,
+            'subTotal' => $subTotal,
+
+            'ivaPerci1' => $ivaPerci1,
+            'ivaRete1' => $ivaRetenida,
+            'reteRenta' => $reteRenta,
+
+            'tributos' => $tributos,
+            'totalLetras' => 'USD ' . $numero_en_letras,
+            'condicionOperacion' => 1,
+        ];
+
+        return $resumen;
+    }
+
+    public static function documentosRelacionados($relacionados)
+    {
+
+        foreach ($relacionados as $value) {
+            $value['tipoGeneracion'] = 2;
+        }
+        return $relacionados;
+    }
+}
--- app/Http/Controllers/DteNdController.php
+++ app/Http/Controllers/DteNdController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Help\DteCodeValidator;
+use App\Help\DTEHelper\CCFDTE;
+use App\Help\DTEHelper\NDDTE;
 use App\Help\DTEIdentificacion\Identificacion;
 use App\Help\DTEIdentificacion\Receptor;
 use App\help\Help;
 use App\Help\LoginMH;
+use App\Help\Services\DteApiMHService;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,49 +19,84 @@ class DteNdController extends Controller
     public function unitario(Request $request)
     {
 
-        // Login para generar token de Hacienda.
+        //PASO 1 , HACER LOGIN EN HACIENDA
         $responseLogin = LoginMH::login();
-        if ($responseLogin['code'] != 200) {
+        if ($responseLogin['code'] != 200)
             return response()->json(DteCodeValidator::code404($responseLogin['error']), 404);
-        }
 
-        $json = $request->json()->all();
+        //PASO 2 OBTENER INFORMACION Y DESFRAGMENTARLA
+        $json = json_decode($request->getContent(), true);
 
         if (!$json) {
             return response()->json(["error" => "DTE no válido o nulo"], Response::HTTP_BAD_REQUEST);
         }
 
-        // VARAIBLES DE CONFIGURACION DEL DTE
-        $dte = $json['dteJson'];
+        if ( ( isset($json['pagoTributos'] ) || $json['pagoTributos']  != null)
+            && count($json['pagoTributos']) != count($json['dteJson']['cuerpoDocumento']) )
+            return request()->json(401, [
+                "msg" => "El campo pagoTributos tiene que tener la misma longitud que cuerpoDocumento"
+            ]);
+
+        $dte = $json['dteJson']; //OBTENER EL DTE
+
+        $pagoTributos = $json['pagoTributos'] ?? null;
+
         $cliente = Help::getClienteId($dte['receptor']['nit']);
-        $tipoDTE = '03';
         $idCliente = $cliente['id'];
+        $newDTE = [];
+        $tipoDTE = '05';
 
-        // VARIABLES DE RESPUESTA DEL SERVICIO
-        $responseData = '';
-        $statusCode = '';
 
-        // Variables de Identificación
-        // $identificacion = Identificacion::identidad($tipoDTE, 3);
-        $contingencia = isset($json['contingencia']) ? $json['contingencia'] : null;
-        $identificacion = Identificacion::identidad($tipoDTE, 3, $contingencia);
+        // return response()->json(Generator::generateNumControl("01"), 200);
 
-        // Variables de Emisor y Receptor
-        $emisor = $dte['emisor'] ?? Identificacion::emisor('03', '20', null);
-        // $receptor = Identificacion::receptorCCF($dte['receptor']);
-        // [$faltan, $receptor] = Receptor::generar($dte['receptor'], $tipoDTE);
+        //PASO 3 GENERAR JSON VALIDO PARA  HACIENDA
+        $identificacion = Identificacion::identidad('05', 3);
+
+
+        $emisor = Identificacion::emisor($tipoDTE, null, null, null);
         [$faltan, $receptor] = Receptor::generar($dte['receptor'], $tipoDTE);
 
-        if ($faltan)
+        if ( $faltan )
             return response()->json($receptor, 404);
 
+        $documentoRelacionado = null;
+        $ventaTercero = null;
+        $cuerpoDocumento = CCFDTE::getCuerpoDocumento($dte['cuerpoDocumento']);
 
-        // return response()->json([
-        //     "identificacion" => $identificacion,
-        //     "emisor" => $emisor,
-        //     "receptor" => $receptor,
-        //     "contingencia" => $contingencia
-        // ], 200);
+        $resumen = null;
+        $extension = null;
+        $apendice = null;
+
+
+        //  return response()->json($dte['documentoRelacionado'], 200);
+        $documentoRelacionado = NDDTE::documentosRelacionados($dte['documentoRelacionado']);
+        $ventaTercero = $dte['ventaTercero'] ?? null;
+        $cuerpoDocumento =  NDDTE::cuerpo($dte['cuerpoDocumento']);
+        $resumen = NDDTE::resumen($cuerpoDocumento, $cliente['tipoCliente'], $pagoTributos);
+        $extension = $dte['extension'];
+        $apendice = $dte['apendice'] ?? null;
+
+        $newDTE = [
+            "identificacion" => $identificacion,
+            "documentoRelacionado" => $documentoRelacionado,
+            "emisor" => $emisor,
+            "receptor" => $receptor,
+            "ventaTercero" => $ventaTercero,
+            "cuerpoDocumento" => $cuerpoDocumento,
+            "resumen" => $resumen,
+            "extension" => $extension,
+            "apendice" => $apendice,
+        ];
+
+        // return $newDTE;
+
+        // PASO 4 MANDAR A HACIENDA EL NUEVO DTE FORMADO SEGUN EL DTE JSON SCHEMA DE LA DOCUMENTACION
+
+        // return response()->json($newDTE, 200);
+
+        [$responseData, $statusCode] = DteApiMHService::envidarDTE($newDTE, $idCliente, $identificacion);
+
+        return response()->json($responseData, $statusCode);
 
     }
 }
